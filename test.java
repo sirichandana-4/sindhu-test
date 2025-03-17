8272bbbcb6680219e77890e14de9b35498417781
sirichandana-4 <siric6677@gmail.com>
2025-03-17 15:28:38 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 			int a = sc.nextInt();
-         		int area = 3.14*a*a;
+         		double area = 3.14*a*a;
 	        	System.out.println(area);
 	}
 }
